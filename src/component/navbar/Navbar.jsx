c54cd168e03c50ea8b8a2f34b72cb5e4066fb7d2
Pritam kumar dash <pritamkumar72002@gmail.com>
2023-02-27 15:48:47 +0530

    problems in sign-up route in responsive

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,60 +1,55 @@
 import React, { useState } from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 import "../../styles/navbar.css";
 
 const Navbar = () => {
-  const [isNavExpanded, setIsNavExpanded] = useState(false);
   const handleLogout = () => {
     localStorage.removeItem("token");
     window.location.reload();
   };
+  const [isMobile, setIsMobile] = useState(false);
+  // const [isSelectNavLink,setIsSelectNavLink]=useState(false);
+  const handleNavClick=()=>{
+    setIsMobile(false);
+  }
   return (
-    <div>
-      <nav className="navigation">
-        <a href="/" className="brand-name">
-          <img src="" alt="navbar Logo" />
-        </a>
-        <button
-          className="hamburger"
-          onClick={() => {
-            setIsNavExpanded(!isNavExpanded);
-          }}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            viewBox="0 0 20 20"
-            fill="white"
-          >
-            <path
-              fillRule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </button>
-        <div
-          className={
-            isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
-          }
-        >
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/sign-up">Sign-Up</Link>
-            </li>
-            <button className="white_btn" onClick={handleLogout}> Logout
+    <div className="navbar-box">
+      <nav className="navbar">
+        <div className="brand-name">
+         navlogo
+        </div>
+          <ul className={isMobile ? "nav-links-mobile" : "nav-links"}
+          onClick={handleNavClick}>
+            <Link to="/" className="nav-links-item">
+               <li>HOME</li>
+            </Link>
+            <Link to="/about" className="nav-links-item">
+              <li>ABOUT US</li>
+            </Link>
+            <Link to="/sign-up" className="nav-links-item">
+              <li>SIGN UP</li>
+            </Link>
+            
+          <button className="white_btn" onClick={handleLogout}> Logout
             </button>
           </ul>
-        </div>
+        <button
+          className="mobile-menu-icon"
+          onClick={() => setIsMobile(!isMobile)}
+        >
+          {isMobile ? <CloseIcon /> : <MenuIcon/>}
+        </button>
       </nav>
     </div>
   );
 };
 
 export default Navbar;
+
+
+
+
+
+
